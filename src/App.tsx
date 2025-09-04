e4a6ceae35cb5b452327b502dcf39c3c31f1f510
anamartinsr <anapribbeiro@gmail.com>
2025-09-04 15:03:49 -0300

    feat(components): add LogoLoop scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import Header from "./components/Header";
+import LogoLoop from "./components/LogoLoop";
+import IconScroll from "/iconScroll.png";
+import TecnlogyScroll from "/tecnologyScroll.png";
 
 function App() {
+  const imageLogos = [
+    { src: IconScroll, alt: "Company 1" },
+    { src: TecnlogyScroll, alt: "Company 1" },
+  ];
+
   return (
     <>
-      <div className="bg-black">
+      <div className="bg-black flex flex-col ">
         <Header />
+
+        <LogoLoop
+          logos={imageLogos}
+          speed={120}
+          direction="left"
+          logoHeight={48}
+          gap={40}
+          pauseOnHover
+          scaleOnHover
+          fadeOut
+          fadeOutColor="#000000"
+          ariaLabel="Technology partners"
+        />
       </div>
     </>
   );
