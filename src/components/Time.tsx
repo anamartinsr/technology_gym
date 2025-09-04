a59f3065eaf7508e904e4047ef53da14e9666474
anamartinsr <anapribbeiro@gmail.com>
2025-09-04 16:06:46 -0300

    feat(components): add Time responsive

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -2,10 +2,10 @@ import ItemTime from "./ItemTime";
 
 export default function Time() {
   return (
-    <div className="bg-black w-full h-40 flex flex-col items-center p-4">
+    <div className="bg-black w-full flex flex-col items-center p-6">
       <p className="text-white font-bold text-3xl">Horários</p>
 
-      <div className="flex flex-row gap-32 justify-between">
+      <div className="flex flex-col sm:flex-row gap-4 sm:gap-32 justify-between">
         <ItemTime day="Segunda - Sexta" time="05:00 / 00:00" />
 
         <ItemTime day="Sábado" time="05:00 / 15:00" />
