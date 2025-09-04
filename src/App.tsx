46be1541cacfadd7577c1db661b1dc3eeacb5f37
anamartinsr <anapribbeiro@gmail.com>
2025-09-04 17:07:47 -0300

    test(app): responsive components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import CardActivitie from "./components/CardActivitie";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import LogoLoop from "./components/LogoLoop";
 import Time from "./components/Time";
 
@@ -14,7 +15,23 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col ">
+      <div className="bg-black">
+        <Header />
+        <Home />
+        <LogoLoop
+          logos={imageLogos}
+          speed={120}
+          direction="left"
+          logoHeight={48}
+          gap={40}
+          pauseOnHover
+          scaleOnHover
+          fadeOut
+          fadeOutColor="#000000"
+          ariaLabel="Technology partners"
+        />
+
+        <Time />
         <CardActivitie />
       </div>
     </>
