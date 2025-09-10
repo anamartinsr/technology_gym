6bb2b2cd3fdff30ec6780997c8d494544f1b2567
anamartinsr <anapribbeiro@gmail.com>
2025-09-10 09:34:53 -0300

    style(components/home): change image height

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 import HomeImg from "/home.jpg";
 
 export default function Home({}) {
-  return <img src={HomeImg} alt="home" className="h-full mt-25" />;
+  return <img src={HomeImg} alt="home" className="h-full mt-20" />;
 }
