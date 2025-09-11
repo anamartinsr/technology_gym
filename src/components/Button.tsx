244cf558bf1c5fce39822aaf1abf44493574c007
anamartinsr <anapribbeiro@gmail.com>
2025-09-11 09:00:56 -0300

    style: add cursor-pointer in button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ const styles = {
 export default function Button({ variant = "primary", text }: ButtonProps) {
   return (
     <button
-      className={`w-full sm:w-auto font-bold py-3 px-6 rounded-full shadow-lg hover:scale-105 transition ${styles[variant]}`}
+      className={`w-full cursor-pointer sm:w-auto font-bold py-3 px-6 rounded-full shadow-lg hover:scale-105 transition ${styles[variant]}`}
     >
       {text}
     </button>
