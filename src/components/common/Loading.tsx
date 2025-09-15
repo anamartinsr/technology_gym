6b3bb08ef6b8c5e8a2216aa1fe3646e233f7b867
Ana <anapribbeiro@gmail.com>
2025-09-15 20:16:30 -0300

    refactor: format with prettier and feat(Loading): add loading, lazy in routes

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-(--secondary-color) text-white">
+      <div className="flex flex-col items-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-(--primary-color) mb-4"></div>
+        <p className="text-lg">Loading...</p>
+      </div>
+    </div>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
 import Layout from "../components/layout/Layout";
-import Home from "../pages/Home";
-import Enrollment from "../pages/Enrollment";
-import NotFound from "../pages/NotFound";
+import Loading from "../components/common/Loading";
+
+const Home = lazy(() => import("../pages/Home"));
+const Enrollment = lazy(() => import("../pages/Enrollment"));
+const NotFound = lazy(() => import("../pages/NotFound"));
 
 export default function AppRoutes() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/enrollment" element={<Enrollment />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Layout>
+    <Suspense fallback={<Loading />}>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/enrollment" element={<Enrollment />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </Suspense>
   );
 }
